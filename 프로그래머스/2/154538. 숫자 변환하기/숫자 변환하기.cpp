d560adcd2e1573cb8531d66cb5994b4cc31d3ef6
LEAAON <luknightsin@gmail.com>
2026-09-21 21:31:24 +0900

    [level 2] Title: 숫자 변환하기, Time: 3.99 ms, Memory: 7.44 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int x, int y, int n)
+{
+    vector<int> dp(y + 1, 1e9);
+    dp[x] = 0;
+
+    int answer = 0;
+    for (int i = x; i <= y; i++)
+    {
+        if (dp[i] == 1e9)
+            continue;
+
+        if (i + n <= y)
+        {
+            dp[i + n] = min(dp[i + n], dp[i] + 1);
+        }
+
+        if (i * 2 <= y)
+        {
+            dp[i * 2] = min(dp[i * 2], dp[i] + 1);
+        }
+
+        if (i * 3 <= y)
+        {
+            dp[i * 3] = min(dp[i * 3], dp[i] + 1);
+        }
+    }
+
+    if (dp[y] == 1e9)
+        answer = -1;
+    else
+        answer = dp[y];
+
+    return answer;
+}
